a1bb10a2b26b9f1110f93aacef08172cbb08d393
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2022-07-01 20:59:49 +0530

    Add files via upload

---
+++ Variables.js
@@ -0,0 +1,33 @@
+ console.log('Variable.js')
+//  Variables in js
+// var, let, const
+var name= 'Varun';
+var channel;
+channel= 'Varun Sharma'
+var marks= 89;
+console.log(name,channel,marks) 
+//Rules for creating for js variables
+/*
+1.Can start with numbers
+2. Can start with letters,numbers, or $
+3. Are Case Sensitive
+*/
+var $city= 'Mumbai'
+console.log($city)
+// Here we use const in place of var 
+const ownersName= 'Sachin';
+console.log(ownersName)
+// ownersName='Ranveer';Cannot do this it gives always Error...
+const fruit= 'Apple';
+console.log(fruit)
+
+// here we use let also
+{
+let carName= 'Mercedes';
+carName='RollsRoyce';
+console.log(carName)
+}
+
+
+
+
