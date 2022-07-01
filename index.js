25269ffe1dbce69d35b9a2b81c47ca8c4e48de8c
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2022-07-01 11:18:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+console.time('Time Taken');
+console.log('Hello Everyone'); 
+console.log(9+9);
+console.table({Varun:'this',marks:87});
+console.warn('This is Warning');
+/* console.timeEnd('Time Taken')*/
