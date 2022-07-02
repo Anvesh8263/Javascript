f9c3250a932e18857fe19a247c8876b229d20a46
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2022-07-02 09:02:58 +0530

    Add files via upload

---
+++ Datatypes.js
@@ -0,0 +1,49 @@
+// Datatypes are of two types 
+// 1.Primitive datatypes----->Stack
+// 2. Reference datatypes---->Heap
+// Primitive datatypes are
+// String
+// Boolean
+// Null
+// undefined
+// Symbol
+// Reference datatypes are
+/*Arrays
+Object literals
+function Date*/
+// String
+var name="Sachin";
+console.log("My String is "  +  name);
+console.log("My Data type is "  +(typeof name));
+// Number
+let marks='87'
+console.log('My Data type is '+(typeof marks));
+//Boolean
+let isDriver=true;
+console.log('My Data type is '+(typeof isDriver));
+//Null
+let nullvar=null;
+console.log('My datatype is '+(typeof
+nullvar));
+//Undefined
+let undef=undefined;
+console.log('My datatype is '+(typeof
+undef));
+
+// Reference Datatypes
+let myarr=[1,2,3,4,5,6,false,"String"];
+console.log('My datatype is '+(typeof
+    myarr));
+// objects literals
+let stmarks ={
+    Sachin:89,
+    Arpit:87,
+    Devesh:88
+} 
+console.log(stmarks);
+function findName(){
+} 
+console.log(typeof findName);
+let date = new Date();
+console.log(typeof date);
+
