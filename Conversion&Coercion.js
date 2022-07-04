f661cf9a6e0ed10354461491434223c9414d7f82
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2022-07-04 08:39:43 +0530

    Add files via upload

---
+++ Conversion&Coercion.js
@@ -0,0 +1,23 @@
+// type coversion and coersion
+
+let myVar;
+myVar=String(34);
+// console.log(myvar,(typeof myVar));
+let booleanVar=String(true);
+// console.log(booleaVar,(typeof booleanVar));
+
+let date=String(new Date());
+// console.log(date, (typeof date));
+
+let arr=String[12,3,4,5,7];
+// console.log(arr.length , (typeof arr));
+
+let i =8;
+console.log(i.toString())
+
+let stri= Number("3434");
+console.log(stri, (typeof stri));
+
+let number=parseFloat('89.0054');
+
+console.log(number.toFixed(2), (typeof number));  
