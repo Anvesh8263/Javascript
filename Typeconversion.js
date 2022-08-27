b1af0454866a8a82940d451da4095867779bf450
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2022-08-27 22:48:32 +0530

    Create Typeconversion.js

---
+++ Typeconversion.js
@@ -0,0 +1,55 @@
+let a=98;
+console.log(a);
+console.log(typeof String(a));
+
+let y =98;
+ a=a.toString();
+console.log(a);
+console.log(typeof a);
+
+let g = 'Anvesh Mangalam';
+console.log(g[7]);
+
+let v = 'Anvesh Mangalam';
+console.log(v.indexOf("M"));
+
+let c = 'Anvesh Mangalam';
+console.log(c.charAt(7));
+
+let d = 'Anvesh Mangalam';
+console.log(d.endsWith('Mangalam'));
+
+let j= 'Anvesh Mangalam';
+console.log(j.includes('Mangalam'));
+
+let z = 'Anvesh Mangalam';
+console.log(z.substring(0,6));
+
+let e = 'Anvesh Mangalam';
+console.log(e.slice(0,6));
+
+let b = 'Anvesh Mangalam';
+console.log(b.split(" "));
+
+let t = 'Anvesh Mangalam';
+console.log(t.replace("Anvesh Mangalam", "AK"));
+
+let bk = 'Anvesh Mangalam';
+console.log(bk.length);
+
+let ek = 'Anvesh Mangalam';
+console.log(ek.toLowerCase());
+
+let ck = 'Anvesh Mangalam';
+console.log(ck.toUpperCase());
+
+r= 'Anvesh Mangalam';
+
+let Heading = document.getElementById("Heading");
+Heading.innerHTML=r;
+
+Heading.style.backgroundColor="yellow";
+Heading.style.borderRadius="20px";
+Heading.style.marginLeft="20px";
+Heading.style.Color="White";
+
