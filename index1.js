45feb99be69fbd32de06eeeb0acf37e2000a688e
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2022-08-27 22:05:29 +0530

    Create index1.js

---
+++ index1.js
@@ -0,0 +1,32 @@
+alert("Hello This is Anvesh Mangalam")
+let a = 120;
+alert(a);
+let b= 'Anvesh Mangalam';
+console.log(b);
+console.log(typeof b);
+let c =33;
+let d= true;
+
+console.log(c);
+console.log(typeof c);
+
+console.log(d);
+console.log(typeof d);
+
+let e = 98;
+
+console.log(e+2); 
+
+let u=98;
+let o= ' Anvesh Mangalam';
+console.log(u+2+o);
+
+let r= 'Anvesh';
+let h= 'Mangalam';
+
+console.log("Hey,My Name is "+r +" "+h+".");
+
+let name= `Anvesh`;
+let surname= `Mangalam`;
+
+console.log(`Hey,My Name is ${name} ${surname}`);
