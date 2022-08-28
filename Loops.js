16621db621d269106206ca7329bc4ab8843c9cce
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2022-08-28 12:49:49 +0530

    Create Loops.js

---
+++ Loops.js
@@ -0,0 +1,34 @@
+oeprators
++,-,*,/,%
+==,>,<,>=,<=,=,!=
+
+let IceCream =Number (prompt("type a Number"));
+if(IceCream===60){
+    console.log('Ice Cream is Cornetto');
+}
+else if(IceCream=== 40){
+    console.log('IceCream is ButterScotch');
+}else{
+    console.log('IceCream is Random')
+}
+
+switch (IceCream){
+    case 60:
+        console.log('Ice Cream is Cornetto');
+
+    break;
+    case 40:
+        console.log('Ice Cream is butterscotch');
+break;
+    default:
+        
+        console.log('Ice Cream is Random');
+        break;
+}
+
+for(let i = 0; i < 10; i=i+1){
+    console.log(Math.random());
+    console.log('Anvesh Mangalam');
+}  
+
+
