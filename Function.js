657cde87697af8c093699c63442db7ff47598811
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2022-08-30 21:39:30 +0530

    Create Function.js

---
+++ Function.js
@@ -0,0 +1,22 @@
+let a = 98;
+let b = 98;
+let c = 98;
+let result = (a*10 + b*10 + c*10)/10;
+
+result=result*2*80;
+console.log(result);
+result=result/60;
+console.log(result);
+
+
+function printTheValue(){
+    let result=(23+23+56+677);
+    result=result*2*80;
+console.log(result);
+result=result/60;
+console.log(result);
+}
+printTheValue();
+printTheValue();
+printTheValue();
+printTheValue();
