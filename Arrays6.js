194746d94168f546591c081579b81b86aa00533b
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2022-08-30 21:13:42 +0530

    Create Arrays6.js

---
+++ Arrays6.js
@@ -0,0 +1,47 @@
+// let arr= [1,2,34,5,6,7,];
+
+// console.log(arr);
+
+
+let asrr=[98,97,69,44,87];
+
+asrr[0]=100;
+asrr[2]=94;
+asrr[4]=78;
+asrr[3]=92;
+
+console.log(asrr);
+
+let arr=[1,2,3,4];
+
+arr.push(5);
+arr.push(6);
+arr.push(7);
+arr.push(8);
+arr.push(9);
+
+console.log(arr);
+// push insert the value at last
+
+let arr1=[1,2,3,4];
+
+arr1.pop();
+arr1.pop();
+arr1.pop();
+
+console.log(arr1);
+// pop rremove the elemnt from the last
+
+let arr2=[1,2,3,4];
+
+arr2.shift();
+
+console.log(arr2);
+//shift remove the element from first
+
+let arr3=[1,2,3,4];
+
+arr3.unshift();
+
+console.log(arr3);
+ 
