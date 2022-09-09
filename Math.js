6122942adaa3ac9427ed1500c179b2fa153cbbe2
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2022-09-09 22:02:32 +0530

    Create Math.js

---
+++ Math.js
@@ -0,0 +1,65 @@
+Math.round(9.8)
+console.log(Math.round(9.8));
+
+const a = Math.round(99.8);
+
+console.log(a);
+
+const b = Math.floor(99.9);
+console.log(b);
+
+const c = Math.ceil(97.8);
+console.log(c);
+
+const d = Math.trunc(99.9);
+console.log(d)
+
+const e = Math.pow(5,2);
+console.log(e);
+
+const f = Math.sqrt(25);
+console.log(f);
+
+const g = Math.abs(5);
+console.log(g);
+
+const funct1 = (a,b)=>{
+    return a -b;
+
+};
+
+console.log(funct1(6,5));
+
+const h = Math.sin((90*3.141592653589793)/180);
+console.log(h); 
+
+const i = Math.PI;
+console.log(i);
+
+const j = Math.sin((90*Math.PI)/180);
+console.log(h); 
+
+const k = Math.random()*10
+console.log(k); 
+
+const l = Math.floor(Math.random()*6+1);
+console.log();
+
+const m = Math.log2(8);
+console.log(m);
+
+const n = new Date();
+console.log(n)
+
+const o = new Date(2010,11,03,12,12,12);
+console.log(o);
+
+const p  = new Date();
+console.log(p.getDate());
+console.log(p.getDay());
+console.log(p.getFullYear());
+console.log(p.getHours());
+console.log(p.getHours());
+console.log(p.getMinutes());
+console.log(p.getSeconds());
+console.log(p.getMilliseconds());
